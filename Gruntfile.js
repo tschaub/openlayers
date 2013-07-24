c84c40b896d4ff216a79a3fb77faa25124643134
Paul Spencer <paul@pagameba.com>
2013-07-23 21:58:14 -0400

    pre-compute sets of files
    The gruntfile now pre-computes sets of files that match the rules
    currently present in build.py.  The files are exposed in the grunt
    configuration object allowing them to be referenced via templates in
    other tasks.  This simplifies setting dependencies for tasks and
    ensures we are using the same rules as build.py.
    
    A simple grunt task to dump a list by name is added for testing
    purposes.  Ideally, it would be modified to execute build.py to dump
    the same file list and do a comparison but I don't know how to modify
    build.py to do this.  For now it will dump to the console if you run
    `grunt listfiles:SRC` and to a file if you run `grunt
    listfiles:SRC:build/src.txt`

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,64 @@ module.exports = function(grunt) {
     return !grunt.file.exists(timestamp) ||
            fs.statSync(timestamp).mtime < fs.statSync(file).mtime;
   }
-
+  var files = {};
+  files.EXPORTS = grunt.file.expand([
+    'src/**/*.exports'
+  ]);
+  files.SRC = grunt.file.expand([
+    'src/ol/**/*.js', 
+    '!src/ol/**/*?shader.js'
+  ]);
+  files.EXTERNAL_SRC = grunt.file.expand([
+    'build/src/external/externs/types.js',
+    'build/src/external/src/exports.js',
+    'build/src/external/src/types.js'
+  ]);
+  files.EXAMPLES = grunt.file.expand([
+    'examples/*.html', 
+    '!examples/index.html'
+  ]);
+  files.EXAMPLES_SRC = grunt.file.expand([
+    'examples/*.js', 
+    '!examples/*.combined.js', 
+    '!examples/bootstrap/*',
+    '!examples/font-awesome/*', 
+    '!examples/Jugl.js', 
+    '!examples/jquery.min.js', 
+    '!examples/loader.js', 
+    '!examples/example-list.js'
+  ]);
+  files.EXAMPLES_JSON = files.EXAMPLES.map(function(f) {
+    return 'build/' + f.replace(/\.html/, '.json');
+  });
+  files.EXAMPLES_COMBINED = files.EXAMPLES.map(function(f) {
+    return 'build/' + f.replace(/\.html/, '.combined.js');
+  });
+  files.INTERNAL_SRC = grunt.file.expand([
+    'build/src/internal/src/requireall.js',
+    'build/src/internal/src/types.js'
+  ]);
+  files.GLSL_SRC = grunt.file.expand([
+    'src/**/*.glsl'
+  ]);
+  files.JSDOC_SRC = grunt.file.expand([
+    'src/**/*.jsdoc'
+  ]);
+  files.SHADER_SRC = grunt.file.expand([
+    'src/**/*?shader.js'
+  ]);
+  files.SPEC = grunt.file.expand([
+    'test/spec/**/*.js'
+  ]);
+  files.LIMITED_DOC_FILES = grunt.file.expand([
+    'externs/*.js', 
+    'build/src/external/externs/*.js'
+  ]);
+  
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    files: files,
     gjslint: {
       source: {
         options: {
@@ -28,14 +82,27 @@ module.exports = function(grunt) {
           }
         },
         files: [{
-          src: ['src/ol/**/*.js', '!src/ol/**/*shader.js'],
+          src: ['<%= files.SRC %>'],
           filter: function(f) {return isNewer(f, 'build/lint-timestamp');}
         }]
+      },
+      generated: {
+        options: {
+          flags:['--jslint_error=all', '--disable=110','--limited_doc_files=<%= files.LIMITED_DOC_FILES %>', '--strict'],
+          reporter: {
+            name: 'console'
+          }
+        },
+        src: ['<%= files.INTERNAL_SRC %>', '<%= files.EXTERNAL_SRC %>'],
+        filter: function(f) { return isNewer(f, 'build/lint-generated-timestamp'); }
       }
     },
     touch: {
       'lint-timestamp': {
         src: ['build/lint-timestamp']
+      },
+      'lint-generated-timestamp': {
+        src: ['build/lint-generated-timestamp']
       }
     }
   });
@@ -49,8 +116,31 @@ module.exports = function(grunt) {
   // alias
   grunt.registerTask('lint', [
     'gjslint:source',
-    'touch:lint-timestamp'
+    'touch:lint-timestamp',
+    'gjslint:generated',
+    'touch:lint-generated-timestamp'
   ]);
+  
+  grunt.registerTask('listfiles', 'helper task to dump out a named list of files (first arg) for comparison to the console or to a file (optional second arg)', function() {
+    var args = Array.prototype.slice.call(arguments);
+    var config;
+
+    if (args.length < 1) {
+      grunt.fatal('specify a list to dump');
+    }
+
+    config = grunt.config('files.'+args[0]);
+    
+    if (!config || !Array.isArray(config)) {
+      grunt.fatal(args[0] + ' is not a valid file list');
+    } else {
+      if (args.length > 1) {
+        grunt.file.write(args[1], config.sort().join('\n'));
+      } else {
+        grunt.log.write(config.sort().join('\n'));
+      }
+    }
+  });
   
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
