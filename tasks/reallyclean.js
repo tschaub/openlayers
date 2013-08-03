bf9fb1d4fc614886c534d14c96441f4e3d161cdd
Paul Spencer <paul@pagameba.com>
2013-08-03 14:20:43 -0400

    Add a really clean task.

---
+++ tasks/reallyclean.js
@@ -0,0 +1,24 @@
+/**
+ * Task to clean build artifacts and to revert to just cloned out state.
+ */
+
+var cmd = require('./lib/cmd');
+
+
+/** @param {Object} grunt Grunt DSL object. */
+module.exports = function(grunt) {
+
+  var description = 'Clean build artifacts.';
+
+  description = 'Removes untracked files and folders from previous builds.'
+  grunt.registerTask('reallyclean', description, function() {
+    var done = this.async();
+    cmd.capture('git', ['clean', '-X', '-d', '-f', '.'], function(err, output) {
+      if (err) {
+        grunt.fatal(err);
+      }
+      grunt.verbose.log(output);
+      done();
+    });
+  });
+};
