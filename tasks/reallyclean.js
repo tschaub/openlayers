a6c47a8b51113cd0d2fe216d778dcc98597158b6
Paul Spencer <paul@pagameba.com>
2013-08-04 20:52:03 -0400

    fix reallyclean target and add missing dependencies to package.json manifest

--- tasks/reallyclean.js
+++ tasks/reallyclean.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
       if (err) {
         grunt.fatal(err);
       }
-      grunt.verbose.log(output);
+      grunt.verbose.writeln(output);
       done();
     });
   });
